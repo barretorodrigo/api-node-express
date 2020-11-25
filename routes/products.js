8d3cc4e3667629c0c6aad5233806870795cb7c66
Rodrigo Barreto <barretorodrigo@gmail.com>
2020-11-25 20:58:43 -0300

    CRUD de produtos e token

--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,63 @@
 const express = require('express');
+const { route } = require('.');
 const router = express.Router();
 const { Product } = require('../models');
 
 // Listar todos os produtos
 router.get('/', async (req, res, next)=>{
-    const products = await Product.findAll();
-    res.json(products);
+    const token = req.headers.authorization;
+    if(token=='ti-ara-2019'){
+        const products = await Product.findAll();
+        res.status(200).json(products);
+    } 
+    res.status(401).send('Token não autorizado');
+    
 });
 
 // Cadastrar produto
 router.post('/', async (req, res, next)=>{
     const product = await Product.create(req.body);
-    res.json(product);
+    res.status(201).json(product);
+});
+
+// Litar produto por id
+router.get('/:id', async (req, res, next)=>{
+    const product = await Product.findAll({
+        where:{
+            id: req.params.id
+        }
+    });
+    res.status(200).json(...product);
+});
+
+// Listar produtos por tipo
+router.get('/type/:type', async(req, res, next)=>{
+    const products = await Product.findAll({
+        where:{
+            type: req.params.type
+        }
+    });
+    res.status(200).json(products);
+});
+
+// Apagar produto por id
+router.delete('/:id', async(req, res, next)=>{
+    const product = await Product.destroy({
+        where:{
+            id: req.params.id
+        }
+    });
+    res.status(204).json(product);
+});
+
+// Atualizar produto por id
+router.put('/:id', async(req, res, next)=>{
+    const product = await Product.update(req.body, {
+        where:{
+            id: req.params.id
+        }
+    });
+    res.status(204).json(product);
 });
 
 module.exports = router;
