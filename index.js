46c4dd8e9ae32ab4f60dbb2710fe39288be308f1
Salmo Marques <salmo.sjunior@sp.senac.br>
2020-11-27 19:06:45 -0300

    ajustes para rodar com o BD local e permitir o acesso do app Web

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const productsRouter = require('./routes/products');
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/clients', clientsRouter);
 app.use('/products', productsRouter);
--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { route } = require('.');
 const router = express.Router();
 const { Product } = require('../models');
 
